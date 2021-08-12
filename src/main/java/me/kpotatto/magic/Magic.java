904b3d1d831013e972e0ef9eba4e56c02ea340e6
KrazyPotatto <kpotatto@gmail.com>
2021-08-12 11:13:07 -0400

    Initial Commit

---
+++ src/main/java/me/kpotatto/magic/Magic.java
@@ -0,0 +1,26 @@
+package me.kpotatto.magic;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Magic implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "magictest";
+    public static final String MOD_NAME = "Test Magic";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
